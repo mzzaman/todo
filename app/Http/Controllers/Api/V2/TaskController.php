b20781f3c73c8103dee75eb8f0354d47c442d932
Moniruzzaman <moniruzzamanm734@gmail.com>
2023-10-10 14:53:04 +0600

    Added a Policy for User Authentication

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -10,6 +10,11 @@ use App\Models\Task;
 
 class TaskController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
     /**
      * Display a listing of the resource.
      */
